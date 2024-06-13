25439c0e45787884fad0cde7ba723f807df21118
NeetigyaPod <neetigya.poddar@amadeus.com>
2024-06-13 12:28:44 +0530

    made the springboot proper

--- src/main/java/com/amadeus/demo/controller/HelloWorldController.java
+++ src/main/java/com/amadeus/demo/controller/HelloWorldController.java
@@ -1,10 +1,10 @@
-package com.geeksforgeeks.controller; 
+package com.amadeus.demo.controller;
 import org.springframework.web.bind.annotation.RequestMapping; 
 import org.springframework.web.bind.annotation.RestController; 
   
 @RestController
 public class HelloWorldController { 
-    @RequestMapping("/") public String helloworld() 
+    @RequestMapping("/") public String helloworld()
     { 
         return "Hello World"; 
     } 
